0a25b9dc2f8f507aa53f7b7b0c3a1390d23ad3df
tksapec <e1399068@gmail.com>
2026-08-28 15:22:16 +0900

    [skip ci] feat: expose database maintenance state

--- FavGCalSchedulerClone.App/ViewModels/MainViewModel.Maintenance.cs
+++ FavGCalSchedulerClone.App/ViewModels/MainViewModel.Maintenance.cs
@@ -7,6 +7,9 @@ public sealed partial class MainViewModel
     private readonly ReminderNotificationService? _reminderService;
     private int _databaseMaintenanceInProgress;
 
+    public bool IsDatabaseMaintenanceInProgress =>
+        Volatile.Read(ref _databaseMaintenanceInProgress) != 0;
+
     public MainViewModel(
         CalendarRepository repository,
         GoogleCalendarSyncService syncService,
@@ -18,5 +21,24 @@ public sealed partial class MainViewModel
         : this(repository, syncService, backupService, csvService, favGCalImportService, logger)
     {
         _reminderService = reminderService;
+    }
+
+    private bool TryBeginDatabaseMaintenanceState()
+    {
+        if (Interlocked.CompareExchange(ref _databaseMaintenanceInProgress, 1, 0) != 0)
+        {
+            return false;
+        }
+
+        OnPropertyChanged(nameof(IsDatabaseMaintenanceInProgress));
+        return true;
+    }
+
+    private void EndDatabaseMaintenanceState()
+    {
+        if (Interlocked.Exchange(ref _databaseMaintenanceInProgress, 0) != 0)
+        {
+            OnPropertyChanged(nameof(IsDatabaseMaintenanceInProgress));
+        }
     }
 }
